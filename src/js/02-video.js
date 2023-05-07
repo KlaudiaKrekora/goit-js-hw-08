9ebfd1c4fa0bc9c08dfd67a0877d13b13bc69af1
KlaudiaKrekora <klaudia.krekora22@gmail.com>
2023-05-07 13:54:44 +0200

    solutions

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function saveTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+const throttle = require('lodash.throttle');
 
+const emailMessItem = document.querySelector('form');
+const emailItem = document.querySelector('input[name="email"]');
+const messageItem = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const data = {
+  email: '',
+  message: '',
+};
+
+const getData = () => {
+  data.email = emailItem.value;
+  data.message = messageItem.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+};
+
+const updateOutput = () => {
+  const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (parsedData) {
+    emailItem.value = parsedData.email;
+    messageItem.value = parsedData.message;
+    data.email = parsedData.email;
+    data.message = parsedData.message;
+  } else {
+    emailItem.value = '';
+    messageItem.value = '';
+  }
+};
+
+const resetData = e => {
+  e.preventDefault();
+  console.log(data);
+  emailMessItem.reset();
+  localStorage.clear();
+};
+
+updateOutput();
+emailMessItem.addEventListener('input', throttle(getData, 500));
+emailMessItem.addEventListener('submit', resetData);
